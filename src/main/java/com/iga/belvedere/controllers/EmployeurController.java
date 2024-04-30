032d1d1bb61770e8dadbe881ba77cf916e4d2d32
FatiMjimer <fatimaezzahra.mjimer@gmail.com>
2024-04-30 20:19:43 +0000

    update post-job

--- src/main/java/com/iga/belvedere/controllers/EmployeurController.java
+++ src/main/java/com/iga/belvedere/controllers/EmployeurController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.iga.belvedere.entities.Catégorie;
 import com.iga.belvedere.entities.Emploi;
 import com.iga.belvedere.entities.Employeur;
+import com.iga.belvedere.entities.Langue;
+import com.iga.belvedere.repositories.LangueRepository;
 import com.iga.belvedere.repositories.catégorieRepo;
 import com.iga.belvedere.repositories.emploiRepository;
 import com.iga.belvedere.repositories.employeurRepository;
@@ -24,24 +26,29 @@ public class EmployeurController {
 	private emploiRepository emploiRepo;
 	@Autowired
 	private catégorieRepo catégorieRepo;
+	@Autowired
+	private LangueRepository langueRepo;
 
 	@GetMapping("/post-job")
 	public String Ajoutjob(Model model) {
 		Emploi newEmploi = new Emploi();
 		List<Employeur> employeurs = employeurRepo.findAll();
 		List<Catégorie> catégories = catégorieRepo.findAll();
+		List<Langue> langues = langueRepo.findAll();
 		model.addAttribute("newEmploi", newEmploi);
 		model.addAttribute("employeurs", employeurs);
 		model.addAttribute("catégories", catégories);
+		model.addAttribute("langues", langues);
 		return "post-job";
 	}
 
 	@PostMapping("/saveEmploi")
-	public String saveEmploi(@RequestParam Catégorie catégorie, @ModelAttribute("newEmploi") Emploi emploi) {
+	public String saveEmploi(@RequestParam Catégorie catégorie,@RequestParam Langue langue, @ModelAttribute("newEmploi") Emploi emploi) {
 		Emploi emplois = new Emploi();
 		Employeur employeur = employeurRepo.findById(1).orElse(null);
 		emploi.setEmployeur(employeur);
 		emploi.setCatégorie(catégorie);
+		emploi.setLangue(langue);
 		emploiRepo.save(emploi);
 
 		// employeur.setId(1);
---
+++ src/main/java/com/iga/belvedere/repositories/LangueRepository.java
@@ -0,0 +1,8 @@
+package com.iga.belvedere.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.iga.belvedere.entities.Langue;
+
+public interface LangueRepository extends JpaRepository<Langue,Integer> {
+
+}
