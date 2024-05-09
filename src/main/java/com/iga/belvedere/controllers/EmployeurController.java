fc55528f31da368b56ca4afe8715dabe20d42db2
Badr JLIL <badr.jlil@outlook.com>
2024-05-09 21:33:44 +0100

    keywords input added, and exigences input changed

--- src/main/java/com/iga/belvedere/controllers/EmployeurController.java
+++ src/main/java/com/iga/belvedere/controllers/EmployeurController.java
@@ -12,11 +12,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.iga.belvedere.entities.Catégorie;
 import com.iga.belvedere.entities.Emploi;
 import com.iga.belvedere.entities.Employeur;
+import com.iga.belvedere.entities.Keyword;
 import com.iga.belvedere.entities.Langue;
 import com.iga.belvedere.repositories.LangueRepository;
 import com.iga.belvedere.repositories.categorieRepository;
 import com.iga.belvedere.repositories.emploiRepository;
 import com.iga.belvedere.repositories.employeurRepository;
+import com.iga.belvedere.repositories.keywordRepository;
 
 @Controller
 public class EmployeurController {
@@ -28,6 +30,8 @@ public class EmployeurController {
 	private categorieRepository catégorieRepo;
 	@Autowired
 	private LangueRepository langueRepo;
+	@Autowired
+	private keywordRepository keywordRepo;
 
 
 	@GetMapping("/post-job")
@@ -44,16 +48,19 @@ public class EmployeurController {
 	}
 
 	@PostMapping("/saveEmploi")
-	public String saveEmploi(@RequestParam Catégorie catégorie,@RequestParam Langue langue, @ModelAttribute("newEmploi") Emploi emploi) {
-		Emploi emplois = new Emploi();
+	public String saveEmploi(@RequestParam Catégorie catégorie,@RequestParam Langue langue, @ModelAttribute("newEmploi") Emploi emploi,
+			@RequestParam String keywordsInput) {
 		Employeur employeur = employeurRepo.findById(1).orElse(null);
 		emploi.setEmployeur(employeur);
 		emploi.setCatégorie(catégorie);
 		emploi.setLangue(langue);
+		String[] keywords = keywordsInput.split(",");
+		for (String keywordStr : keywords) {
+		    Keyword keyword = new Keyword(keywordStr.trim());
+		    keyword.setEmploi(emploi);
+		    keywordRepo.save(keyword);
+		}
 		emploiRepo.save(emploi);
-
-		// employeur.setId(1);
-		// emplois.setEmployeur(employeur);
 
 		return "redirect:/";
 	}
